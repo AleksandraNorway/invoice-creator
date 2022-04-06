484b1b8d321e00a262b69af2a4e7d2d7d7920f82
AleksandraNorway <phimaster.ai@gmail.com>
2022-04-06 21:09:32 +0200

    new

---
+++ index.js
@@ -0,0 +1,106 @@
+const btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+const amount = document.getElementById("amount")
+const amount2 = document.getElementById("amount2")
+const amount3 = document.getElementById("amount3")
+let totalAmount = document.getElementById("total-amount")
+let text1 = document.getElementById("text1")
+let text2 = document.getElementById("text2")
+let text3 = document.getElementById("text3")
+let sum = 0
+let fisrtPrice = 10
+let secondPrice = 20 
+let thirdPrice = 30
+let textHTML1 = "Wash Car"
+let textHTML2 = "Mow Lawn"
+let textHTML3 = "Pull Weeds"
+let amountSpan = document.getElementById("amount-span")
+let amount2Span = document.getElementById("amount2-span")
+let amount3Span = document.getElementById("amount3-span")
+
+let total = []
+let total1 = []
+let total2 = []
+
+function Add(price) {
+    return sum += price
+}
+
+
+
+btn1.addEventListener("click", function() {
+
+   amount.innerHTML =  ` 
+   <span style="color: #918E9B" >$</span>${fisrtPrice} `
+    Add(fisrtPrice)
+    totalAmount.textContent = "$" + sum
+    text1.textContent = textHTML1
+    total2.push(fisrtPrice)
+    generateTotal(total2)
+    
+    //generateTotal(total)
+  
+ 
+   for (let i = 0; i < total2.length; i++) {
+
+        amountSpan.innerHTML = total2.length
+    
+   }
+  
+})
+
+btn2.addEventListener("click", function() {
+
+   amount2.innerHTML =  `
+    <span style="color: #918E9B" >$</span>${secondPrice} `
+    Add(secondPrice)
+    totalAmount.textContent = "$" + sum
+    text2.textContent = textHTML2
+    total1.push(secondPrice)
+    generateTotal(total1)
+    
+    //generateTotal(total)
+  
+ 
+   for (let i = 0; i < total1.length; i++) {
+
+        amount2Span.innerHTML = total1.length
+    
+   }
+ 
+ 
+    
+})
+btn3.addEventListener("click", function() {
+  
+    amount3.innerHTML =  ` 
+    <span style="color: #918E9B" >$</span>${thirdPrice} `
+    Add(thirdPrice)
+    totalAmount.textContent = "$" + sum
+    text3.textContent = textHTML3 
+    total.push(thirdPrice)
+    //generateTotal(total)
+  
+ 
+   for (let i = 0; i < total.length; i++) {
+
+        amount3Span.innerHTML = total.length
+    
+   }
+ 
+  
+})
+
+function generateTotal(arr) {
+   let bla 
+    for (let i = 0; i < arr.length; i++) {
+        bla += arr[i] 
+    }
+   return bla
+   
+
+}
+
+
+
