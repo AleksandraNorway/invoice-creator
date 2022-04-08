f52c73f344198907b43e15798a85b3eee33ac394
Aleksandra Ilic <95101761+AleksandraNorway@users.noreply.github.com>
2022-04-08 10:04:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
 
 const amount = document.getElementById("amount")
 const amount2 = document.getElementById("amount2")
@@ -17,10 +17,6 @@ let fisrtPrice = 10
 let secondPrice = 20 
 let thirdPrice = 30
 
-let textHTML1 = "Wash Car"
-let textHTML2 = "Mow Lawn"
-let textHTML3 = "Pull Weeds"
-
 let amountSpan = document.getElementById("amount-span")
 let amount2Span = document.getElementById("amount2-span")
 let amount3Span = document.getElementById("amount3-span")
@@ -28,83 +24,55 @@ let amount3Span = document.getElementById("amount3-span")
 let total = []
 let total1 = []
 let total2 = []
+
 let container = document.getElementById("container")
 
 function Add(price) {
-    return sum += price
+     sum += price
+     let ourText = `<p id="color1">$${sum} <p> `
+     totalAmount.innerHTML = ourText
+     totalAmount.style.color = "#4A4E74"; 
+   
 }
-
+  
 
 
 btn1.addEventListener("click", function() {
     amount.innerHTML = ` <span style="color: #918E9B" >$</span>${fisrtPrice} `
     Add(fisrtPrice)
-    totalAmount.innerHTML = `<p id="color1">$${sum} <p> `
-    text1.textContent = textHTML1
+    text1.textContent = "Wash Car"
     total2.push(fisrtPrice)
-    totalAmount.style.color = "#4A4E74"; 
-  
-  
- 
-   for (let i = 0; i < total2.length; i++) {
-
+    
+ for (let i = 0; i < total2.length; i++) {
        amountSpan.innerHTML = total2.length
-    
-   }
-    
-  
+   }   
+ 
 })
+
 
 btn2.addEventListener("click", function() {
 
-  amount2.innerHTML =  ` <span style="color: #918E9B" >$</span>${secondPrice} `
+    amount2.innerHTML =  ` <span style="color: #918E9B" >$</span>${secondPrice} `
     Add(secondPrice)
-    totalAmount.textContent = sum
-text2.textContent = textHTML2
+    text2.textContent = "Mow Lawn"
     total1.push(secondPrice)
-    //generateTotal(total1)
-    
-    //generateTotal(total)
   
- 
+  
    for (let i = 0; i < total1.length; i++) {
-
-        amount2Span.innerHTML = total1.length
-    
-   }
- 
-
-    
-})
-btn3.addEventListener("click", function() {
-
-    amount3.innerHTML = "$" + thirdPrice
-    Add(thirdPrice)
-    totalAmount.textContent = sum
-    text3.textContent = textHTML3 
-    total.push(thirdPrice)
-    //generateTotal(total)
+     amount2Span.innerHTML = total1.length
+     }
   
+     
+})
+
+btn3.addEventListener("click", function() {
+    amount3.innerHTML =  ` <span style="color: #918E9B" >$</span>${thirdPrice} `
+    Add(thirdPrice)
+
+    text3.textContent = "Pull Weeds" 
+    total.push(thirdPrice)
  
    for (let i = 0; i < total.length; i++) {
-
-        amount3Span.innerHTML = total.length
-    
-   }
+     amount3Span.innerHTML = total.length
+     }
 })
-
-
-
-function remove() {
-    element.remove();
-}
-
-/* function generateTotal(arr) {
-   let bla 
-    for (let i = 0; i < arr.length; i++) {
-        bla += arr[i] 
-    }
-   return bla
-
-}
-*/
